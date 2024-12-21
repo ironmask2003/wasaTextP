735a72e544647b6e7ce59ca470205992ce8eb86d
ironmask2003 <lachithaperera75@gmail.com>
2024-12-21 01:51:00 +0100

    Modified module

--- cmd/webapi/register-web-ui.go
+++ cmd/webapi/register-web-ui.go
@@ -4,10 +4,10 @@ package main
 
 import (
 	"fmt"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/webui"
 	"io/fs"
 	"net/http"
 	"strings"
+	"wasa.project/webui"
 )
 
 func registerWebUI(hdl http.Handler) (http.Handler, error) {
